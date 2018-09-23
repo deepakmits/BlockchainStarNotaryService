f68edb9de2f9e49d6e3e294386272315725fe7c8
deepakmits <deepakmits@yahoo.com>
2018-09-23 20:06:37 +0530

    bug fixes

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ server.route({
 				.required(),
 			}
 		},
-		
+
 		handler:async (request,h) => {
 			let stars = [];
 			let add = request.params.address;
@@ -127,7 +127,7 @@ server.route({
 				.required(),
 			}
 		},
-		
+
 		handler:async (request,h) => {
 			let hash = request.params.hash;
 			let retBlock = null;
@@ -135,12 +135,12 @@ server.route({
 				return currHeight;
 			});
 			console.log(currentBlockchainHeight);
-			
+
 			for(let i=0;i<=currentBlockchainHeight;i++){
 				let currBlock = await deepChain.getBlock(i).then((block) => {
 					return block;
 				});
-				
+
 				console.log('Hash : '+JSON.stringify(currBlock));
 				if(currBlock.hash === hash){
 					var storyDecode = new Buffer(currBlock.body.star.story, 'hex');
@@ -172,39 +172,11 @@ server.route({
 	}
 });
 
-//http://localhost:8000/chain/block/0 
-//server.route({
-//	method:'GET',
-//	path:'/chain/block/{height}',
-//	config:{
-//		tags: ['api'],
-//		description: 'Get Block at a given height',
-//		notes: 'Get Block at a given height, returns Block as JSON',
-//		validate: {
-//			params: {
-//				height : Joi.number()
-//				.required(),
-//			}
-//		},
-//		handler:async (request, h) => {
-//			let height = encodeURIComponent(request.params.height);
-//			let retBlock = await deepChain.getBlock(height).then((block) => {
-//				return block;
-//			}).catch((err) => {
-//				console.log('Deep chain could not find required block '+height , err);
-//				return errorHandler(request, h, 'Deep chain could not find required block '+height);
-//			});
-//
-//			return h.response(retBlock).type('application/json');
-//		}
-//	}
-//});
-
 
 //http://localhost:8000/chain/block/0 
 server.route({
 	method:'GET',
-	path:'/stars/block/{height}',
+	path:'/block/{height}',
 	config:{
 		tags: ['api'],
 		description: 'Get Star at a given height',
@@ -241,38 +213,74 @@ server.route({
 		description: 'Add Star to current Blockchain',
 		notes: 'Adds Star to current Blockchain, passing a block data as payload => {"body":"Block data"}',
 		validate: {
-			payload: {
-				body : Joi.object()
-				.required(),
-			}
+			payload: Joi.object()
+			.required(),
 		},
 		handler:async (request, h) => {
 			if(request.payload == null || 
-					(request.payload != null && (typeof request.payload.body  == 'undefined' 
-						|| encodeURIComponent(request.payload.body).length == 0))
-			)
+					(request.payload != null && (typeof request.payload  == 'undefined' 
+						|| encodeURIComponent(request.payload).length == 0))
+			){
 				return errorHandler(request, h, 'Block with empty body cannot be created/added.');
+			}
 
-			let blockBody = request.payload.body;
-			blockBody.star.story = new Buffer(blockBody.star.story).toString('hex');
-			let newBlock = new Block(blockBody);
-			let retBlock = await deepChain.addBlock(newBlock).then((block) => {
-				return block;
-			}).catch((err) => {
-				console.log('Deep chain could not add block', err);
-				return errorHandler(request, h, 'Deep chain could not add block '+err);
-			});
+			var ascii = /^[ -~\t\n\r]+$/;
 
-			return h.response(retBlock).type('application/json');
+			let blockBody = request.payload;
+			if(blockBody.address == null || blockBody.star  == null || blockBody.star ==='undefined' ||
+					blockBody.star.dec == null || blockBody.star.dec.length == 0 || 
+					blockBody.star.ra == null || blockBody.star.ra.length == 0 ||
+					blockBody.star.story == null || blockBody.star.story.length == null ||
+					new Buffer(blockBody.star.story).length > 500 || !ascii.test(blockBody.star.story)){
+				return errorHandler(request, h, 'one of dec, ra and story mandatory fields is missing OR story length is more than 500'
+						+' bytes OR story contains non-ascii characters');
+			}
+			
+			let valReqBlock = await getValidationRequest(blockBody.address);
+			if(valReqBlock.registerStar == undefined || !valReqBlock.registerStar){
+				return errorHandler(request, h, 'No valid request found OR request is timed out OR you have already registerd star using this existing request for address '+blockBody.address +'. Please create one before registering your star.');
+			}else{
+				blockBody.star.story = new Buffer(blockBody.star.story).toString('hex');
+				let newBlock = new Block(blockBody);
+				let retBlock = await deepChain.addBlock(newBlock).then((block) => {
+					return block;
+				}).catch((err) => {
+					console.log('Star chain could not add block', err);
+					return errorHandler(request, h, 'Star chain could not add block '+err);
+				});
+				
+				valReqBlock.registerStar = false;
+				let val = await srdb.put(address, JSON.stringify(valReqBlock)).then((value)=>{
+					console.log('Invalidating current request after registering a Star.');
+					return JSON.stringify(valReqBlock);
+				}).catch((err => {
+					console.log();
+					errorHandler(request, h, 'Invalidating Request could not be added ',err);
+				}));
+				
+				return h.response(retBlock).type('application/json');
+			}
 		}
 	}
 });
 
 
+function getValidationRequest(address){
+	return new Promise( async (resolve,reject)=>{
+		let existingValidationReq = await srdb.get(address).then((block) => {
+			console.log('got block with address : '+block);
+			resolve(JSON.parse(block));
+		}).catch((err) => {
+			console.log('Validation request could not be found with given address.', err);
+			resolve(err);
+		});
+	});
+}
+
 //http://localhost:8000/chain/message-signature/validate
 server.route({
 	method:'POST',
-	path:'/chain/message-signature/validate',
+	path:'/message-signature/validate',
 	config:{
 		tags: ['api'],
 		description: 'Validates the signature and establishes identity of submitter.',
@@ -296,7 +304,7 @@ server.route({
 
 			let address = encodeURIComponent(request.payload.address);
 			let signature = request.payload.signature;			
-			
+
 			let existingValidationReq = await srdb.get(address).then((block) => {
 				console.log('got block with address : '+block);
 				if(block.address === '')
@@ -319,34 +327,35 @@ server.route({
 					let verified = false;
 					try{
 						verified = bitcoinMessage.verify(existingVR.message,address,signature);
+						if(verified){
+							console.log('got request : '+JSON.stringify(existingVR));
+							jsonResponse = {
+									"registerStar": true,
+									"status": {
+										"address": address,
+										"requestTimeStamp": existingVR.requestTimeStamp,
+										"message": existingVR.message,
+										"validationWindow": existingVR.validationWindow,
+										"messageSignature": "valid"
+									}
+							}
+
+							let val = await srdb.put(address, JSON.stringify(jsonResponse)).then((value)=>{
+								console.log('Returning req after adding.')
+								return JSON.stringify(validationRequest);
+							}).catch((err => {
+								console.log();
+								errorHandler(request, h, 'Validation Request could not be added ',err);
+							}));
+
+						}else{
+							errorHandler(request, h, 'Message could not be verified with Address and Signature.');
+						}
 					}catch(err){
 						console.log(err);
 						errorHandler(request, h, 'Message could not be verified with Address and Signature.',err);
 					}
-					if(verified){
-						console.log('got request : '+JSON.stringify(existingVR));
-						jsonResponse = {
-								"registerStar": true,
-								"status": {
-									"address": address,
-									"requestTimeStamp": existingVR.requestTimeStamp,
-									"message": existingVR.message,
-									"validationWindow": existingVR.validationWindow,
-									"messageSignature": "valid"
-								}
-						}
-					}else{
-						jsonResponse = {
-								"registerStar": false,
-								"status": {
-									"address": address,
-									"requestTimeStamp": existingVR.requestTimeStamp,
-									"message": existingVR.message,
-									"validationWindow": existingVR.validationWindow,
-									"messageSignature": "invalid"
-								}
-						}
-					}
+
 				}else{
 					errorHandler(request, h, 'Valid Request not found.');
 				}
@@ -362,7 +371,7 @@ server.route({
 //http://localhost:8000/chain/requestValidation
 server.route({
 	method:'POST',
-	path:'/chain/requestValidation',
+	path:'/requestValidation',
 	config:{
 		tags: ['api'],
 		description: 'Adds validation request and returns the validation request',
@@ -398,12 +407,12 @@ server.route({
 				valReq.requestTimeStamp = validationRequest.requestTimeStamp;
 				valReq.message = validationRequest.message;
 				valReq.validationWindow = validationRequest.validationWindow;
-				if(valReq.isRequestValid())
-					valReq.updateValidationRequest();
-				else{
-					console.log('Setting address to empty as request is not valid.');
-					valReq.address = '';
+				if(valReq.isRequestValid()){
+					valReq.validationWindow = valReq.remainingTimeWindow();
+				}else{
+					valReq = new ValidationRequest(address); 
 				}
+
 				validationRequest = valReq;
 			}
 			let val = await srdb.put(address, JSON.stringify(validationRequest)).then((value)=>{
